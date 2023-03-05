79f44bc2da840587fa5993e927146de825da3cb3
dionannd <dionananda77@gmail.com>
2023-03-05 22:44:10 +0700

    fix(index): change url my personal blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ const Home = () => {
             back-end to become a fullstack developer, I Experienced in using
             Laravel and CodeIgniter frameworks, and I&apos;m also learning other
             things like Golang, Flutter. Record traces by writing in{' '}
-            <Link href='https://blog-dion.vercel.app' target='_blank'>
+            <Link href='https://blog.dianananda.site' target='_blank'>
               My personal blog
             </Link>
             .

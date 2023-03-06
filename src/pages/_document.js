838f4eb86f133bd71c0f6fa350288dd5862aba1e
dionannd <dionananda77@gmail.com>
2023-03-06 12:17:32 +0700

    fix(google-site): change meta google-site id verifikasi

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,29 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+          />
+          <script // eslint-disable-next-line
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+          <meta
+            name='google-site-verification'
+            content='oGMcQ3blcc6X_PKLS4qMOfah9a-QYb4oDzroIt_sKUQ'
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
